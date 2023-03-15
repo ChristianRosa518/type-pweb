b5ac69f565c91ef8f29994a06a669095a804f6ad
ChristianRosa518 <chrisrosa815518@gmail.com>
2023-03-15 06:26:58 -0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
   function appHeight() {
     const doc = document.documentElement;
-    doc.style.setProperty("--vh", window.innerHeight * 0.01 + "px");
+    doc.style.setProperty("--vho", window.innerHeight * 0.01 + "px");
   }
 
   window.addEventListener("resize", appHeight);
